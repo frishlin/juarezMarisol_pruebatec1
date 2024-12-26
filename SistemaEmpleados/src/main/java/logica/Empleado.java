2cd4fc2a58a138afa926796d6418671350f755f8
frishlin <frishlin@gmail.com>
2024-12-26 04:17:33 -0600

    Creacion de clase Persona y conexion BD

---
+++ SistemaEmpleados/src/main/java/logica/Empleado.java
@@ -0,0 +1,65 @@
+
+package logica;
+
+import java.time.LocalDate;
+
+public class Empleado {
+    private String nombre;
+    private String apellido;
+    private String cargo;
+    private double salario;
+    private LocalDate fechaInicio;
+
+    public Empleado() {
+    }
+
+    public Empleado(String nombre, String apellido, String cargo, double salario, LocalDate fechaInicio) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.cargo = cargo;
+        this.salario = salario;
+        this.fechaInicio = fechaInicio;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getCargo() {
+        return cargo;
+    }
+
+    public void setCargo(String cargo) {
+        this.cargo = cargo;
+    }
+
+    public double getSalario() {
+        return salario;
+    }
+
+    public void setSalario(double salario) {
+        this.salario = salario;
+    }
+
+    public LocalDate getFechaInicio() {
+        return fechaInicio;
+    }
+
+    public void setFechaInicio(LocalDate fechaInicio) {
+        this.fechaInicio = fechaInicio;
+    }
+    
+    
+}
